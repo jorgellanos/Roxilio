3c3d29c41e0ef3c036d91a2d284128ba69929384
jorgellanos <jorg.llanos@hotmail.com>
2018-11-30 20:40:01 -0300

    More Scripts
    Cree un script que fucniona con el htc y uno sin htc para probarlo en la casa

---
+++ Assets/DashVR.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class DashVR : MonoBehaviour {
+
+    public float distance;
+    public Transform pointer;
+    public bool isMoving;
+    public float speed;
+    public GameObject marker, stop;
+    private GameObject go;
+    public RaycastHit hit;
+    private Ray landingRay;
+
+    // Use this for initialization
+    void Start()
+    {
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        landingRay = new Ray(pointer.position, pointer.forward);
+
+        if (Physics.Raycast(landingRay, out hit, distance))
+        {
+            // Marker
+            Destroy(go);
+            go = Instantiate(marker, hit.point, Quaternion.identity);
+            go.transform.position = hit.point;
+        }
+
+        VRMethod();
+
+        if (Input.GetKeyDown("space"))
+        {
+            Destroy(stop);
+            stop = Instantiate(marker, hit.point, Quaternion.identity);
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            Move(stop.transform);
+        }
+    }
+
+    public void Move(Transform target)
+    {
+
+        float moving = speed * Time.deltaTime;
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, new Vector3(target.position.x, 0, target.position.z), moving);
+        if (Vector3.Distance(transform.localPosition, target.position) <= 1)
+        {
+            isMoving = false;
+        }
+    }
+
+    public void VRMethod()
+    {
+        if (SteamVR_Input._default.inActions.Teleport.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            isMoving = true;
+            Destroy(stop);
+            stop = Instantiate(stop, hit.point, Quaternion.identity);
+            stop.transform.position = go.transform.position;
+        }
+        
+    }
+}
