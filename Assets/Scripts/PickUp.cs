7a8c899e15a947d274bb79fb4818a0013c437d93
jorgellanos <jorg.llanos@hotmail.com>
2019-01-11 14:26:44 -0300

    ahora si
    yes

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,13 +4,62 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool grabbed;
+    public float dist;
+
+    // Use this for initialization
+    void Start()
+    {
+        grabbed = false;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("grabed " + gameObject.name);
+            grabbed = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            grabbed = false;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("grabed " + gameObject.name);
+            grabbed = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            grabbed = false;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+        Vector3 objeto = Camera.main.ScreenToWorldPoint(mouse);
+        if (grabbed)
+        {
+            transform.position = objeto;
+            Scroll();
+        }
+    }
+
+    public void Scroll()
+    {
+        float distChange = 1f;
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            dist -= distChange * Time.deltaTime * 10;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            dist += distChange * Time.deltaTime * 10;
+        }
+    }
 }
