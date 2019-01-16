4501295919241dcf6de62aa78444d30a03f8f8af
jorgellanos <jorg.llanos@hotmail.com>
2019-01-16 15:56:22 -0300

    STEAMSTUUFFFF
    EFQWT

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Interact : MonoBehaviour {
+
+    [HideInInspector]
+    public Hand activeHand = null;
+
+
+}
