77d777051cf47df296a4e4634cc4eda80f424d5b
jorgellanos <jorg.llanos@hotmail.com>
2019-01-17 15:02:39 -0300

    Examined
    examinar las cosas POh

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -9,5 +9,56 @@ public class Interact : MonoBehaviour {
     [HideInInspector]
     public Hand activeHand = null;
 
+    public bool examined;
+    private TextMesh text;
+
+    private void Start()
+    {
+        text = GetComponentInChildren<TextMesh>();
+
+        text.text = string.Empty;
+        examined = false;
+    }
+
+    private void Update()
+    {
+        if (examined)
+        {
+            Examine();
+        }
+        else
+        {
+            text.text = string.Empty;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "hand")
+        {
+            examined = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "hand")
+        {
+            examined = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "hand")
+        {
+            examined = false;
+        }
+    }
+
+    public void Examine()
+    {
+        text.text = gameObject.name;
+    }
 
 }
