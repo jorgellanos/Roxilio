b10a511697c4af563f9bd000cdab9ee385e74186
Ethese <f.goyeneche.camus@gmail.com>
2019-01-23 15:23:43 -0300

    TouchpadMove redy

--- Assets/Scripts/Vida/MovementTouchpad.cs
+++ Assets/Scripts/Vida/MovementTouchpad.cs
@@ -7,10 +7,8 @@ public class MovementTouchpad : MonoBehaviour
 {
     [SteamVR_DefaultAction("MoveTouchpad")]
     public float speed;
-    Vector2 movimiento;
-    public Rigidbody rb;
-    public GameObject ojos;
-    bool keyUp;
+    private Vector2 movimiento = Vector2.zero;
+    public Transform cameraRig;
 
     void Start()
     {
@@ -24,38 +22,11 @@ public class MovementTouchpad : MonoBehaviour
 
     void Movement()
     {
-        if (movimiento.x > 0 || movimiento.y > 0)
-        {
-            keyUp = true;
-        }
+        //cameraRig.transform.eulerAngles = new Vector3(0, rotation, 0);
         movimiento = SteamVR_Input._default.inActions.MoveTouchpad.GetAxis(SteamVR_Input_Sources.Any);
-        Vector3 movement;
-        if (keyUp)
-        {
-            movement = new Vector3(movimiento.x, 0.0f, movimiento.y);
-            rb.MovePosition(movement * speed);
-        }
-        {
-
-        }
-
-            //Debug.Log(movement);
-        
-        /*if (transform.parent.position.y < 0)
-        {
-            movimiento = SteamVR_Input._default.inActions.MoveTouchpad.GetAxis(SteamVR_Input_Sources.Any);
-            movement = new Vector3(-movimiento.x, 0.0f, movimiento.y);
-            rb.AddForce(movement * speed);
-            //Debug.Log(movement);
-        }
-        if (transform.parent.position.y > 0)
-        {
-            movimiento = SteamVR_Input._default.inActions.MoveTouchpad.GetAxis(SteamVR_Input_Sources.Any);
-            movement = new Vector3(movimiento.x, 0.0f, -movimiento.y);
-            rb.AddForce(movement * speed);
-            //Debug.Log(movement);
-        }*/
-        keyUp = false;
-
+        //cameraRig.transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        //rb.MovePosition(transform.position + (transform.right * movimiento.x * speed) + (transform.forward * movimiento.y * speed));
+        cameraRig.position += (transform.right * movimiento.x + transform.forward * movimiento.y) * Time.deltaTime * speed;
+        cameraRig.position = new Vector3(cameraRig.position.x , 0, cameraRig.position.z);
     }
 }
