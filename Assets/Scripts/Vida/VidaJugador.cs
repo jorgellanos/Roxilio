1b56ebd3f777d258e3875c216cc2be012c52dc8f
Ethese <f.goyeneche.camus@gmail.com>
2019-01-24 15:55:08 -0300

    24-01-2019

--- Assets/Scripts/Vida/VidaJugador.cs
+++ Assets/Scripts/Vida/VidaJugador.cs
@@ -14,6 +14,7 @@ namespace vida
         public int currentLifes;
         bool isDead;                                                // Whether the player is dead.
         public Text uiVida;
+        public Text uiVidas;
         float timer;
         float timeToRespawn = 15f;
 
@@ -23,6 +24,7 @@ namespace vida
             currentHealth = startingHealth;
             currentLifes = startingLifes;
             uiVida.text = ("Vida: " + currentHealth);
+            uiVidas.text = ("Vidas: " + currentLifes);
 
         }
 
@@ -42,6 +44,7 @@ namespace vida
             // Reduce the current health by the damage amount.
             currentHealth -= amount;
             uiVida.text = ("Vida: " + currentHealth);
+
             // If the player has lost all it's health and the death flag hasn't been set yet...
             if (currentHealth <= 0 && !isDead)
             {
@@ -66,22 +69,31 @@ namespace vida
         void Death()
         {
             // Set the death flag so this function won't be called again.
+            anim.SetTrigger("FadeOut");
             uiVida.text = ("Vida: " + "0");
             isDead = true;
             currentLifes = currentLifes - 1;
-            anim.SetTrigger("FadeOut");
         }
 
         void Respawn()
         {
-            if (isDead && currentLifes >= 0)
+            if (isDead && currentLifes >= 0 )
             {
+                uiVidas.text = ("Vidas: " + currentLifes);
                 currentHealth = 100;
                 Debug.Log("Moriste" + currentLifes);
                 anim.SetTrigger("FadeIn");
                 uiVida.text = ("Vida: " + currentHealth);
                 isDead = false;
             }
+        }
+
+        private IEnumerator WaitForAnimation(Animation animation)
+        {
+            do
+            {
+                yield return null;
+            } while (animation.isPlaying);
         }
     }
 }
