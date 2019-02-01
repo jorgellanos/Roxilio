6e4d0ee73fb25f9056d4d37e8bc0a6004c018968
Puppiesonac1d <the_sorr0w@hotmail.com>
2019-02-01 15:30:51 -0300

    Update - Clima, Zoom

---
+++ Assets/Resources/Scripts/Sonido.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonido : MonoBehaviour
+{
+    public AudioClip Clip;
+    public AudioSource Fuente;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fuente.clip = Clip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fuente.Play();
+    }
+}
